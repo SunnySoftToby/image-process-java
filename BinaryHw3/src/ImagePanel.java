a65812742766141cfdffd6cea9651b78b767730e
TobyYang <yangme9@gmail.com>
2022-09-17 20:42:39 +0800

    binary image

---
+++ BinaryHw3/src/ImagePanel.java
@@ -0,0 +1,10 @@
+import java.awt.Graphics;
+import javax.swing.JPanel;
+
+public class ImagePanel extends JPanel {
+	boolean readyToPaint;
+
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+	}
+}
